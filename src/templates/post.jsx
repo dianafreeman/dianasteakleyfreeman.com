f0b0a5380d8586f79575edf308eea82f4cc2397a
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2019-08-25 20:08:00 -0500

    makes post template legible

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -1,42 +1,103 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import PageLayout from '../layouts/Layout'
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import Img from 'gatsby-image'
+import { graphql, Link } from 'gatsby';
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons.cjs';
+import Layout from '../layouts/Layout';
+import { colors } from '../../tailwind';
+// Components
+import TypedTitle from '../components/TypedTitle';
+import Content from '../elements/Content';
+import Inner from '../elements/Inner';
 
-const PostTemplate = ({ data }) => {
-  const post = data.wordpressPost
-  console.log(data)
+const PageTitleWrapper = styled(ParallaxLayer)`
+  ${tw`my-auto px-2`}
+  background-image: url('${props => props.bg}');
+  background-size: cover;
+  background-position: center;
+`;
+
+const PostWrapper = styled.div`
+  ${tw`relative mt-4`}
+  width: 100%;
+  height: 100% !important;
+  background-color: rgba(0, 0, 0, 0.7);
+  &:before{
+    content: ' ';
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+  }
+`;
+
+const PostContentLayer = styled(ParallaxLayer)`
+ background-image: url('${props => props.bg}');
+  background-size: cover;
+  background-position: center;
+  
+`
+
+const PostContentWrapper = styled.div`
+  ${tw`px-2 py-3 mx-auto w-full mx-2`}
+  background: lightgray;
+  min-height: fit-content;
+
+`
+
+const BlogPostTemplate = ({ data }) => {
+  const post = data.wordpressPost;
   return (
-    <PageLayout title={post.title}>
-      <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
-    </PageLayout>
-  )
-}
+    <Layout>
+      <Helmet title={`${post.title} | Blog`} />
+      <Parallax pages={3} factor={1}>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <Img fluid={post.featured_media.localFile.childImageSharp} />
+          <TypedTitle strings={[post.title]} />
+        </ParallaxLayer>
+        <PostContentLayer offset={0.5} speed={0.5} factor={3}>
+          <PostContentWrapper>
+            <article dangerouslySetInnerHTML={{ __html: post.content }} style={{display: 'block'}}/>
+          </PostContentWrapper>
+        </PostContentLayer>
+        <Link to="/">Back to Home</Link>
+      </Parallax>
+    </Layout>
+  );
+};
 
-export default PostTemplate
+BlogPostTemplate.propTypes = {
+  data: PropTypes.objectOf('element').isRequired
+};
 
-PostTemplate.propTypes = {
-  data: PropTypes.array.isRequired,
-}
+export default BlogPostTemplate;
 
-// export const pageQuery = graphql`
-//   query wordpressPost(slug: $slug) {
-//     featured_media {
-//       localFile {
-//         childImageSharp {
-//           resolutions {
-//             src
-//           }
-//         }
-//       }
-//     }
-//     title
-//     internal {
-//       content
-//     }
-//     slug
-//     wordpress_id
-//   }
-// }
-
-// `
+export const query = graphql`
+  query($id: Int!) {
+    wordpressPost(wordpress_id: { eq: $id }) {
+      id
+      title
+      content
+      date
+      slug
+      categories
+      wordpress_id
+      featured_media {
+        localFile {
+          url
+          childImageSharp {
+            fluid(maxWidth: 960) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
