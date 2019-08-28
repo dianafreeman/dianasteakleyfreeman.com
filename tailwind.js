c242953de839b9515292bacfc5cc4d8307db9db9
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2019-08-28 15:26:51 -0500

    adds brand colors

--- tailwind.js
+++ tailwind.js
@@ -46,6 +46,18 @@ const defaultConfig = require('tailwindcss/defaultConfig')();
 */
 
 const colors = {
+  brands: {
+    'adobe-creative-suite': '#fbb034',
+    twitter: '#1da1f2',
+    github: '#333',
+    javascript: '#f7df1e',
+    node: '#215732',
+    grunt: '#fba919',
+    wordpress: '#21759b',
+    gatsby: '#b300c9',
+    sass: '#E35BE5',
+    medium: '#00ab6c'
+  },
   transparent: 'transparent',
   'blue-black': '#161719',
   'blue-grey': '#23262b',
