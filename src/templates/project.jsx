1b5141cbb86d48f72218fcfb49cd03be43a6cd97
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2019-09-03 22:00:01 -0500

    cleans dependencies, updates props

--- src/templates/project.jsx
+++ src/templates/project.jsx
@@ -1,92 +1,129 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
 import Img from 'gatsby-image'
-import { graphql, Link } from 'gatsby';
+import { graphql, StaticQuery}  from 'gatsby';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
-import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons.cjs';
+import FastAverageColor from 'fast-average-color';
+import { ParallaxLayer, Parallax } from 'react-spring/renderprops-addons.cjs'
 import Layout from '../layouts/Layout';
-import { colors } from '../../tailwind';
-// Components
+import { colors } from '../../tailwind'
 import TypedTitle from '../components/TypedTitle';
+import icondata from '../../config/icondata'
 
 
-const PageTitleWrapper = styled.div`
-  ${tw`absolute pin-t`}
+// const TechnologyIcon = (id) => (
+//   <StaticQuery
+//     query={graphql`
+//     query TechQuery {
+//       allWordpressWpTechnologies {
+//         edges {
+//           node {
+//             wordpress_id
+//             name
+//             slug
+//           }
+//         }
+//       }
+//     }
+    
+//     `}
+//     render={techData => (
+//       <header>
+//         {techData.allWordpressWpTechnologies.edges.map( tech => <h2>{tech.node.name}</h2>)}
+//       </header>
+//     )}
+//   />
+// );
+  
+
+const PageHeader = styled.div`
+${tw`relative w-full`}
 `;
 
-const HeaderImage = styled(Img)`
- ${tw`absolute h-screen`}
- 
-`
 const ArticleWrapper = styled.div`
-  ${tw`p-3 mx-auto rounded-lg w-5/6`}
-  background: rgba(225,225,225,0.9);
-  height: inherit;
-  overflow: hidden scroll;
+${tw`py-3 px-3 mx-auto rounded-sm my-3 relative`}
+@media screen and (min-width: 568px){width:  95vw };
+background: rgba(225,225,225,0.9);
+display: block;
 `
-const PageContent = styled.div`
-  ${tw` relative `}
+const TitleWrapper = styled.div`
+${tw`text-left rounded-md w-full`}
+  padding-top: 90px;
+  background-color: rgba(0,0,0,0.5);
+  right: 0;
+  bottom: 1em;
+  z-index: 1;
+  h1  {
+    text-shadow: 5px 5px 3px black !important;
+  }
 `
 
-const BlogPostTemplate = ({ data }) => {
+const ProjectPageTemplate = ({data}) => {
   const project = data.wordpressWpProjects;
+  const technologies = data.allWordpressWpTechnologies.edges
+    .filter( tech => project.technologies.includes(tech.node.wordpress_id))
+    .map( edge => edge.node);
+    
   return (
-    <Layout>
-      <Helmet title={`${project.title} | Blog`} />
-      <Parallax pages={2}>
-        <ParallaxLayer offset={0}>
-          <HeaderImage fluid={project.featured_media.localFile.childImageSharp.fluid} />
-          <PageTitleWrapper>
-            <Link to="/">Back to Home</Link>
-            <TypedTitle strings={[project.title]} />
-          </PageTitleWrapper>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.25} factor={1.5}>
-          <ArticleWrapper>
-            <article>
-              <div dangerouslySetInnerHTML={{ __html: project.content }} style={{display: 'block'}}/> 
-            </article>
-          </ArticleWrapper>
-        </ParallaxLayer>
-      
-      </Parallax>
+    <Layout parallax={false}>
+      <PageHeader>
+        <TitleWrapper>
+          <TypedTitle strings={[project.title]}/>
+        </TitleWrapper>
+      </PageHeader>
+      <ArticleWrapper>
+        <Img fluid={project.featured_media.localFile.childImageSharp.fluid} style={{width: '300px', marginRight: '1em', float: 'left'}}/>
+        <article>
+          <div dangerouslySetInnerHTML={{ __html: project.content }} style={{display: 'block'}}/> 
+        </article>
+        <aside>
+          <p>This app uses...{ technologies.map( tech => icondata.filter()`${tech.name}, `) }</p>
+        </aside>
+      </ArticleWrapper>
     </Layout>
-  );
-};
+  )
+}
 
-BlogPostTemplate.propTypes = {
-  data: PropTypes.objectOf('element').isRequired
-};
+ProjectPageTemplate.propTypes = {
+  data: PropTypes.objectOf('element').isRequired,
+}
 
-export default BlogPostTemplate;
+export default ProjectPageTemplate
 
 export const query = graphql`
   query($id: Int!) {
     wordpressWpProjects(wordpress_id: { eq: $id }) {
-        acf {
-            project_color
-            project_link
+      acf {
+        project_color
+        project_link
+      }
+      featured_media {
+        localFile {
+          url
+          childImageSharp {
+            fluid(maxWidth: 960) {
+              ...GatsbyImageSharpFluid
+            }
           }
-          featured_media {
-            localFile {
-                url
-                childImageSharp {
-                  fluid(maxWidth: 960) {
-                     ...GatsbyImageSharpFluid
-                    }
-                }
-              }
-            id
-          }
-          content
-          title
-          slug
-          technologies
+        }
+        id
+      }
+      content
+      title
+      slug
+      technologies
+      wordpress_id
+    }
+    allWordpressWpTechnologies {
+      edges {
+        node {
           wordpress_id
+          name
+          slug
+        }
+      }
     }
   }
-`;
+`
