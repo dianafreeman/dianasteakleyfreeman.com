d3b0aa509c74e7bb32649ec4dfcb2dbe54e7c71e
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2020-01-21 21:32:15 -0600

    adds react spring

--- .storybook/config.js
+++ .storybook/config.js
@@ -9,7 +9,7 @@ configure(require.context("../src", true, /\.stories\.js$/), module)
 
 /* DECORATORS */
 const Theme = story => (
-    <div style={{ width: "95vw", height: '95vh', backgroundColor: colors.background }}>
+    <div style={{ width: "95vw", height: '95vh', margin: "auto", justifyContent: 'space-evenly', backgroundColor: colors.background , textAlign: "center"}}>
       {story()}
     </div>
   );
--- src/components/CardGrid/CardGrid.jsx
+++ src/components/CardGrid/CardGrid.jsx
@@ -1,20 +1,39 @@
 /* eslint-disable react/require-default-props */
-import React from 'react';
+
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { useTransition, useTrail, animated } from 'react-spring';
 import PropTypes from 'prop-types';
 import { Link, graphql } from 'gatsby';
+import styled from 'styled-components';
 import Card from '../Card';
 
+const AniCard = ({ children , styles, ...restProps}) => {
+  const Wrapper = animated(Card);
+  return <Wrapper {...restProps}/>
+}
+const items = ['item a', 'item b', 'item c', 'item d', 'item e', 'item f'];
+
 const CardGrid = ({ props }) => {
-  const items = ['a', 'b', 'c', 'd', 'e', 'f'];
+  const [toggle, set] = useState(true)
+  const config = { mass: 5, tension: 2000, friction: 200 }
+
+  const trail = useTrail(items.length, {
+    config,
+    o: toggle ? 1 : 0,
+    xyz: toggle ? [0, 400, 0] : [0, 0, 0],
+    from: { x: 0, h: 0 },
+  })
+
+
   return (
-    <div style={{ flexFlow: 'row wrap', display: 'flex', margin: 'auto' }}>
-      {items.map(i => (
-        <Card key={`card-key-${i}`} title={i} />
-      ))}
+    <div onClick={() => set(state => !state)}>
+      {trail.map(({ xyz, o,  ...rest }, index) => (
+            <AniCard key={items[index]} styles={{ opacity: o.interpolate(o => `${o}`)}} style={{ transform: xyz.interpolate((x,y,z) => `perspective(100vw) translate3d(${x}px,${y}px,${z}px)`) }} title={items[index]} />
+        ))}
     </div>
   );
 };
-
 // CardGrid.propTypes = {
 
 // };
