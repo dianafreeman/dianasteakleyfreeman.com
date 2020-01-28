a7b87beb19c8192ec6cfd02d557d6b918a72db2c
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2020-01-27 21:19:09 -0600

    update tests

--- src/components/CardGrid/CardGrid.spec.jsx
+++ src/components/CardGrid/CardGrid.spec.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import renderer, { act } from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import CardGrid from '.';
+import { default as DATA }  from '../../config/__fixtures__/cardGridData';
 
 describe('CardGrid', () => {
+
   it('should render without exploding', () => {
-    expect(render(<CardGrid />)).not.to.throw;
+    act( () => {
+      expect(render(<CardGrid items={{...DATA}} />)).not.to.throw;
+    })
   });
 
   it('should match snapshot', () => {
-    const tree = renderer.create(<CardGrid />).toJSON();
-    jestExpect(tree).toMatchSnapshot();
+      const tree = renderer.create(<CardGrid items={{...DATA}} />).toJSON();
+      jestExpect(tree).toMatchSnapshot();
+
   });
 });
