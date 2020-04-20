d322415acf0057d61fad19c2068dc80dc2e05dde
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2020-04-19 21:15:15 -0500

    update config

--- helpers/scripts/__templates__/DianaBookES7/COMPONENT_NAME.jsx
+++ helpers/scripts/__templates__/DianaBookES7/COMPONENT_NAME.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { inject, observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-
 
 const Wrapper = styled.div``;
 
@@ -17,4 +15,4 @@ COMPONENT_NAME.propTypes = {
   ]).isRequired,
 };
 
-export default inject('store')(observer(COMPONENT_NAME));
+export default COMPONENT_NAME;
