790431c9a4b655dc41c8e71c70c58edaa7d5fa11
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2020-04-23 15:33:21 -0500

    add context for nav and theme

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,35 +1,70 @@
-import React from 'react';
-import { inject, observer } from 'mobx-react';
+import React, { useContext } from 'react';
 import { useTrail } from 'react-spring';
+import styled from '@emotion/styled';
+import { css, jsx } from '@emotion/core';
+import { ThemeContext } from '../../context/ThemeContext';
 import POST from './post.json';
-import { Image, ArticleWrap, Content, Body, Icon } from './styled';
 import MetaBox from './MetaBox';
 
-const Article = ({ post = POST, title = 'A Title', store, ...restProps }) => {
-  const items = { timeToRead: post.timeToRead, tags: post.tags, date: post.frontmatter.date };
+export const Image = styled.img`
+  width: 100%;
+  margin: auto;
+  @media screen and (min-width: ${theme.screen.md}) {
+    float: right;
+    width: 45%;
+    margin: 0 1em;
+  }
+`;
+
+// export const ArticleWrap = styled.article`
+
+// `;
+// export const Content = styled.div`
+//   color: white;
+// `;
+// export const Body = styled.div`
+//   padding-top: 10px;
+// `;
+
+// export const Icon = styled.span`
+//   padding-right: 0.5em;
+// `;
+/** @jsx jsx */
+const Article = ({ post = POST, store, ...restProps }) => {
+  // const items = { timeToRead: post.timeToRead, tags: post.tags, date: post.frontmatter.date };
+  const [theme, setTheme] = useContext(ThemeContext);
 
   return (
-    <ArticleWrap {...restProps}>
-      <Content>
-        <Body>
-          <MetaBox
-            items={[
-              {
-                label: `⏱ Time to read`,
-                value: `${post.timeToRead} minutes`,
-              },
-              {
-                label: `Published On`,
-                value: `${post.frontmatter.date}`,
-              },
-            ]}
-          />
+    <article
+      {...restProps}
+      css={css`
+        padding: 1em;
+        width: inherit;
+        background-color: ${theme.color.dark};
+      `}
+    >
+      <div
+        css={css`
+          color: white;
+        `}
+      >
+        <MetaBox
+          items={[
+            {
+              label: `⏱ Time to read`,
+              value: `${post.timeToRead} minutes`,
+            },
+            {
+              label: `Published On`,
+              value: `${post.frontmatter.date}`,
+            },
+          ]}
+        />
 
-          <Image src="https://via.placeholder.com/500x300" alt="" />
-          <div dangerouslySetInnerHTML={{ __html: post.html }} style={{ display: 'block' }} />
-        </Body>
-      </Content>
-    </ArticleWrap>
+        <Image src="https://via.placeholder.com/500x300" alt="" />
+        <div dangerouslySetInnerHTML={{ __html: post.html }} style={{ display: 'block' }} />
+      </div>
+    </article>
   );
 };
 
---
+++ src/context/ThemeContext/theme/icondata.js
@@ -0,0 +1,85 @@
+/* eslint-disable react/display-name */
+import React from 'react';
+import tinycolor from 'tinycolor2';
+import {
+  FaGrunt,
+  FaAdobe,
+  FaAccessibleIcon,
+  FaTwitter,
+  FaMediumM,
+  FaGithubAlt,
+  FaSass,
+  FaJs,
+} from 'react-icons/fa';
+import { colors } from './tailwind';
+
+const getTextColor = color => (tinycolor(color).isLight() ? 'black' : 'white');
+const defaultIconProps = { size: '35px' };
+const iconData = [
+  {
+    label: 'Accessibility Controls',
+    slug: 'accessibility',
+    color: colors['blue-dark'],
+    icon: (() => <FaAccessibleIcon {...defaultIconProps} />)(),
+    textColor: (() => getTextColor(colors['blue-dark']))(),
+    link: null,
+  },
+  {
+    label: 'Adobe Creative Suite',
+    slug: 'adobe-creative-suite',
+    color: colors.brands['adobe-creative-suite'],
+    icon: (() => <FaAdobe {...defaultIconProps} />)(),
+    textColor: (() => getTextColor(colors.brands['adobe-creative-suite']))(),
+    link: 'https://www.adobe.com/creativecloud/catalog/desktop.html',
+  },
+  {
+    label: 'Grunt',
+    slug: 'grunt',
+    color: colors.brands.grunt,
+    icon: (() => <FaGrunt {...defaultIconProps} />)(),
+    textColor: (() => getTextColor(colors.brands.grunt))(),
+    link: 'https://gruntjs.com/',
+  },
+  {
+    label: 'Twitter',
+    slug: 'twitter',
+    color: colors.brands.twitter,
+    icon: (() => <FaTwitter {...defaultIconProps} />)(),
+    textColor: (() => getTextColor(colors.brands.twitter))(),
+    link: 'https://twitter.com/dianasfreeman/',
+  },
+  {
+    label: 'Medium',
+    slug: 'twitter',
+    color: colors.brands.medium,
+    icon: (() => <FaMediumM {...defaultIconProps} />)(),
+    textColor: (() => getTextColor(colors.brands.medium))(),
+    link: 'https://medium.com/dianasfreeman/',
+  },
+  {
+    label: 'GitHub',
+    slug: 'github',
+    color: colors.brands.github,
+    icon: (() => <FaGithubAlt {...defaultIconProps} />)(),
+    textColor: (() => getTextColor(colors.brands.github))(),
+    link: 'https://github.com/dianafreeman/',
+  },
+  {
+    label: 'SASS',
+    slug: 'sass',
+    color: colors.brands.sass,
+    icon: (() => <FaSass {...defaultIconProps} />)(),
+    textColor: (() => getTextColor(colors.brands))(),
+    link: 'https://sass-lang.com/',
+  },
+  {
+    label: 'JavaScript',
+    slug: 'javascript',
+    color: colors.brands.javascript,
+    icon: (() => <FaJs {...defaultIconProps} />)(),
+    textColor: (() => getTextColor(colors.brands))(),
+    link: 'https://sass-lang.com/',
+  },
+];
+
+export default iconData;
