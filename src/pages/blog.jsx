e6d3af83b0d565bad5b9b5c03ccae154264fd9b6
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2020-05-07 21:52:49 -0500

    update construction messages on all routes

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
-import { Parallax } from 'react-spring/renderprops-addons.cjs';
-import PageLayout from '../layouts/Layout';
-import PostList from '../components/PostList';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import { Parallax } from 'react-spring/renderprops-addons.cjs'
+import PageLayout from '../layouts/Layout'
+import PostList from '../components/PostList'
 
-const BlogPage = (posts) => (
+const BlogPage = posts => (
   <PageLayout>
-    {/* <PostList posts={posts} title="Latest posts" /> */}
+    <Row>
+      <BigTitle style={{ paddingTop: '25vh' }}>Diana M Steakley Freeman</BigTitle>
+    </Row>
+    <Row>
+      <Subtitle style={{ color: 'white', margin: 'auto', fontStyle: 'italic', fontSize: '1.5em' }}>
+        Hang tight -- a brand new site is coming soon!
+      </Subtitle>
+    </Row>{' '}
   </PageLayout>
-);
+)
 
-export default BlogPage;
+export default BlogPage
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -28,11 +28,9 @@ const Index = () => {
       <Row>
         <BigTitle style={{paddingTop: '25vh'}}>Diana M Steakley Freeman</BigTitle>
       </Row>
-
       <Row>
         <Subtitle style={{color: 'white', margin: "auto", fontStyle: "italic", fontSize: '1.5em'}}>Hang tight -- a brand new site is coming soon!</Subtitle>
       </Row>
-
     </PageLayout>
   );
 };
