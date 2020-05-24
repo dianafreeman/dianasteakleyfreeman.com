73a4b7c8db20a598d777175511594a51fcdd62a7
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2020-05-24 12:02:34 -0500

    restore under construction default

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import { Router } from '@reach/router';
-import Blog from '../components/Pages/Blog';
-import Home from '../components/Pages/Home';
+// import Blog from '../components/Pages/Blog';
+// import Home from '../components/Pages/Home';
 import NotFound from '../components/Pages/NotFound';
-
-const LazyComponent = ({ PageComponent, ...props }) => (
-  <React.Suspense fallback={'<p>Loading...</p>'}>
-    <PageComponent {...props} />
-  </React.Suspense>
-);
+import ComingSoon from '../components/Pages/ComingSoon';
 
 const Index = () => {
-  return (
-    <Router>
-      <LazyComponent PageComponent={Home} path="/" />
-      <LazyComponent PageComponent={Blog} path="blog" />
-      <LazyComponent PageComponent={NotFound} default />
-    </Router>
-  );
+  return <ComingSoon />;
 };
 export default Index;
