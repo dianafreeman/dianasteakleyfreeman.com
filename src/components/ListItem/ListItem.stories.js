8a70018b7cc73eac343360f1e85e30dc7390b80d
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2020-05-24 12:04:45 -0500

    format, prepare for under construction deploy

--- src/components/ListItem/ListItem.stories.js
+++ src/components/ListItem/ListItem.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import ListItem from '.';
 
-export const byDefault = () =>  <ListItem  />
+export const byDefault = () => <ListItem />;
 
 export default {
   title: 'ListItem',
