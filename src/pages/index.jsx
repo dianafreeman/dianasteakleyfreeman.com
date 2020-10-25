09a6ebd03dd067fb9749f98a58bdbcba9a47296f
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2020-10-25 16:31:38 -0500

    cleanup before redesign

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 const Home = () => {
   return (
-    <div>
-      <div className="col-sm-6 " style={{ display: 'flex', margin: 'auto' }}>
-        <div className="mx-auto">
-          <h1>Hi. I'm Diana. 👋</h1>
-          <p style={{ color: 'white' }}>
-            Cillum esse aliquip occaecat eu sunt fugiat sunt nisi. Elit sit aliqua tempor aliquip id
-            nisi non veniam. Ullamco quis dolor irure dolore non laborum. Eu sunt ullamco magna nisi
-            est laborum. Non pariatur amet aliquip laboris excepteur sit reprehenderit reprehenderit
-            occaecat dolor quis sint incididunt incididunt. Pariatur ullamco pariatur enim ea.
-          </p>
-        </div>
+    <Layout>
+      <div>
+        <h1>Hi. I'm Diana. 👋</h1>
+        <p style={{ color: 'white' }}>
+          Cillum esse aliquip occaecat eu sunt fugiat sunt nisi. Elit sit aliqua tempor aliquip id
+          nisi non veniam. Ullamco quis dolor irure dolore non laborum. Eu sunt ullamco magna nisi
+          est laborum. Non pariatur amet aliquip laboris excepteur sit reprehenderit reprehenderit
+          occaecat dolor quis sint incididunt incididunt. Pariatur ullamco pariatur enim ea.
+        </p>
       </div>
-      <div className="col-sm-6" style={{ display: 'flex' }}>
-        tkjhalkjh
-      </div>
-    </div>
+    </Layout>
   );
 };
+
 export default Home;
