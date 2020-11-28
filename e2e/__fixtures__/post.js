23a23e47204c599dc735de36a6833da6892c80c8
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2020-11-27 21:08:52 -0600

    add cypress e2e directory

---
+++ e2e/__fixtures__/post.js
@@ -0,0 +1,20 @@
+import htmlAsT from './htmlAst';
+
+export default {
+  node: {
+    frontmatter: {
+      path: '/blog/digital-self-care',
+      date: '2019-03-31',
+      slug: 'digital-self-care',
+      title: 'Digital Self-Care in the era of #InsertCrisisHere',
+    },
+    timeToRead: 4,
+    headings: [
+      {
+        value: 'Digital Self-Care in the era of #InsertCrisisHere',
+        depth: 1,
+      },
+    ],
+    htmlAsT,
+  },
+};
