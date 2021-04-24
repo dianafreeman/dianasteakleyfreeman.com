e25242bd91c331b48a6343d5367888456a4983f4
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2021-04-23 21:07:20 -0500

    format

--- src/html.js
+++ src/html.js
@@ -7,7 +7,10 @@ export default function HTML(props) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
         {props.headComponents}
 
         <link
@@ -23,7 +26,11 @@ export default function HTML(props) {
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
-        <div key="body" id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
+        <div
+          key="body"
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
         {props.postBodyComponents}
       </body>
     </html>
