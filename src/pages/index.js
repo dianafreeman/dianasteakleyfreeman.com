a2ded8ca76e0f6e0baf7070cc54ddfb17f45747b
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2021-04-24 18:14:06 -0500

    update site map, google anlaytics  (#10)
    * add logging options

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import Scene from '../components/Scene';
 
 function Home() {
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
+    <div style={{ height: '100vh' }}>
       <Scene />
     </div>
   );
