b4865504247f876ea169df766baf050706f07b30
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2021-12-28 14:57:49 -0600

    delete extra stuff

--- src/hooks/useLayout/index.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { LayoutContext } from "../../providers/LaptopProvider";
-
-const useLayout = () => useContext(LayoutContext);
-
-export default useLayout;
