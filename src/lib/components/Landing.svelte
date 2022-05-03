7675784e65d555e2723d8c74c99d52f767b606b1
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-05-03 18:28:09 -0500

    add sliding transitions

--- src/lib/components/Landing.svelte
+++ src/lib/components/Landing.svelte
@@ -1,45 +1,31 @@
 <script>
   import SlidesStore from "$stores/SlidesStore";
-  import { fade, blur, fly, slide, scale } from "svelte/transition";
-
+import { onMount } from "svelte";
+  import { spring } from "svelte/motion";
+  import LandingSlide from "./LandingSlide.svelte";
 
   let slides, activeIndex;
 
-
-  $: activeSlide = slides[activeIndex];
-
+  let left = spring(0);
+  let height 
+  
+  let offset;
   SlidesStore.subscribe((data) => {
     slides = data.slides;
     activeIndex = data.activeIndex;
-
+    left.set(data.activeIndex * 100);
   });
-  $: displayNumber = `0${activeIndex + 1}.`;
-  $: isLongWord = activeSlide.title.length > 8;
+
 </script>
 
-{#key activeIndex}
-  <div>
-    <div
-      class="flex flex-col md:flex-row flex-1 align-middle justify-center px-5"
-      style="min-height: 70vh;"
-    >
-      <div class="flex my-auto flex-col p-5 md:m-auto md:w-1/2" style="min-width: 50%;">
-        <h1
-          class="font-bold md:text-center {isLongWord
-            ? 'text-5xl lg:text-6xl'
-            : 'text-6xl lg:text-7xl'}"
-        >
-          {activeSlide.title}
-        </h1>
-      </div>
-      <div class="flex my-auto flex-col p-5 md:m-auto md:w-1/2">
-        <h2 class="font-manrope font-black text-5xl lg:text-6xl py-3">{displayNumber}</h2>
-        <p class="font-manrope text-xl lg:text-2xl ">{activeSlide.description}</p>
-      </div>
-    </div>
-    <div class="flex flex-row justify-between align-middle w-full px-4">
-      <button class="p-4 m-2" on:click={SlidesStore.previous}>PREV</button>
-      <button class="p-4 m-2" on:click={SlidesStore.next}>NEXT</button>
-    </div>
+<div class="w-screen overflow-hidden relative" style="height: 85vh">
+  <div class="absolute" style="height: inherit; width: calc(100vw * {slides.length}); left: -{$left}vw;">
+    {#each slides as slide, i}
+      <LandingSlide {slide} index={i} />
+    {/each}
   </div>
-{/key}
+  <div class="absolute bottom-0 flex flex-row justify-between align-middle w-full px-4">
+    <button class="p-4 m-2" on:click={SlidesStore.previous}>PREV</button>
+    <button class="p-4 m-2" on:click={SlidesStore.next}>NEXT</button>
+  </div>
+</div>
---
+++ src/lib/components/LandingSlide.svelte
@@ -0,0 +1,32 @@
+<script>
+  import SlidesStore from "$stores/SlidesStore";
+  // import { fade, blur, fly, slide, scale } from "svelte/transition";
+
+  export let slide;
+  export let index;
+
+  $: displayNumber = `0${index + 1}.`;
+  $: isLongWord = slide.title.length > 8;
+</script>
+
+<div class="absolute w-full" style="height: inherit; left: {index * 100}vw">
+  <div class="inline-flex w-screen" style="height: 70%;">
+    <div
+      class="flex flex-col md:flex-row flex-1 align-middle justify-center px-5 h-full"
+    >
+      <div class="flex my-auto flex-col p-5 md:m-auto md:w-1/2" style="min-width: 50%;">
+        <h1
+          class="font-bold md:text-center {isLongWord
+            ? 'text-5xl lg:text-6xl'
+            : 'text-6xl lg:text-7xl'}"
+        >
+          {slide.title}
+        </h1>
+      </div>
+      <div class="flex my-auto flex-col p-5 md:m-auto md:w-1/2">
+        <h2 class="font-manrope font-black text-5xl lg:text-6xl py-3">{displayNumber}</h2>
+        <p class="font-manrope text-xl lg:text-2xl ">{slide.description}</p>
+      </div>
+    </div>
+  </div>
+</div>
