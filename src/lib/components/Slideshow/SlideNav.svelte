d28d39ca8be23125768396a8bb89ad2adaebf0ee
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-05-28 17:19:26 -0500

    add focus states to slide nav buttons

--- src/lib/components/Slideshow/SlideNav.svelte
+++ src/lib/components/Slideshow/SlideNav.svelte
@@ -6,7 +6,7 @@
   <button class="p-3" on:click={SlidesStore.previous}>PREV</button>
   <div class="mx-2 inline-flex my-auto">
     {#each $SlidesStore.landingSlides as slide, i}
-      <button class="m-1 rounded-full bg-white w-3 h-3" />
+      <button class="m-1 rounded-full border hover:bg-white focus:bg-white w-3 h-3" />
     {/each}
   </div>
   <button class="p-3" on:click={SlidesStore.next}>NEXT</button>
