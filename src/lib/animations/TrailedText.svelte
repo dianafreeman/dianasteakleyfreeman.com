61982e834c05f22056cfb99b84dfe789066e212c
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-05-28 21:22:57 -0500

    format

--- src/lib/animations/TrailedText.svelte
+++ src/lib/animations/TrailedText.svelte
@@ -3,7 +3,6 @@
   import Trail from "./Trail/Trail.svelte";
 
   import TrailStore from "$stores/TrailStore";
-
 </script>
 
 <span class="sr-only">The future of engineering is human. Hi, I'm Diana.</span>
