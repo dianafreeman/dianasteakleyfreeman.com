35f80df6f4a203155fd40a5b9c948d40bb1842e7
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-05-29 18:55:29 -0500

    upgrade sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -5,14 +5,13 @@ import adapter from "@sveltejs/adapter-auto";
 const config = {
   kit: {
     adapter: adapter(),
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
     vite: {
       resolve: {
         alias: {
           $lib: path.resolve("./src/lib"),
-          $stores: path.resolve("./src/stores")
+          $stores: path.resolve("./src/stores"),
+          $content: path.resolve("./src/content"),
+          $fonts: path.resolve("./src/fonts")
         }
       }
     }
