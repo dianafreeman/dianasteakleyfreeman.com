17ae12a9dd8c92831a00b6e546d4430ae4668e22
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-06-20 12:41:30 -0500

    add buttonGroup Variants

---
+++ src/lib/components/ButtonGroup/FlatButtonGroup.svelte
@@ -0,0 +1,74 @@
+<script>
+  import LayoutStore from "$stores/LayoutStore";
+  import SolidCircleLeft from "$lib/icons/solidCircleLeft.svelte";
+  import SolidCircleUp from "$lib/icons/solidCircleUp.svelte";
+  import SolidCircleDown from "$lib/icons/solidCircleDown.svelte";
+  import SolidCircleRight from "$lib/icons/solidCircleRight.svelte";
+  import ContentStore from "$stores/ContentStore";
+  import { derived } from "svelte/store";
+
+  const { incrementRow, decrementRow, incrementCol, decrementCol } = LayoutStore;
+
+  const actionButtonGroups = derived(LayoutStore, ($LayoutStore) => {
+    return [
+      [
+        {
+          id: "scrollLeft",
+          text: "left",
+          value: { x: $LayoutStore.x - 1, y: $LayoutStore.y },
+          component: SolidCircleLeft,
+          fn: decrementCol
+        }
+      ],
+      [
+        {
+          id: "scrollUp",
+          text: "up",
+          value: { y: $LayoutStore.y - 1, x: $LayoutStore.x },
+          component: SolidCircleUp,
+          fn: decrementRow
+        }
+      ],
+      [
+        {
+          id: "scrollDown",
+          text: "down",
+          value: { y: $LayoutStore.y + 1, x: $LayoutStore.x },
+          component: SolidCircleDown,
+          fn: incrementRow
+        }
+      ],
+      [
+        {
+          id: "scrollRight",
+          text: "right",
+          value: { x: $LayoutStore.x + 1, y: $LayoutStore.y },
+          component: SolidCircleRight,
+          fn: incrementCol
+        }
+      ]
+    ];
+  });
+</script>
+
+<div class="fixed bottom-0 w-full" style:z-index={99999}>
+  <div class="flex justify-evenly align-middle h-28">
+    {#each $actionButtonGroups as buttonGroup}
+      <div
+        class="flex flex-col"
+        class:justify-center={buttonGroup.length === 1}
+        class:justify-between={buttonGroup.length > 1}
+      >
+        {#each buttonGroup as button}
+          <button
+            class="text-green-500 disabled:text-red-500 h-10 lowercase font-thin px-3 disabled:line-through"
+            disabled={!ContentStore.cellExists(button.value)}
+            on:click={button.fn}
+          >
+            {button.text}
+          </button>
+        {/each}
+      </div>
+    {/each}
+  </div>
+</div>
