a1790c3e261bab821392242aef364c26a85b4f89
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-07-02 17:48:37 -0500

    Add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,27 @@
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
 import path from "path";
 import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: [".svelte", ...mdsvexConfig.extensions],
+
   kit: {
     adapter: adapter(),
     vite: {
       resolve: {
         alias: {
-          $lib: path.resolve("./src/lib"),
+          $routes: path.resolve("./src/routes"),
           $stores: path.resolve("./src/stores"),
           $content: path.resolve("./src/content"),
           $fonts: path.resolve("./src/fonts")
         }
       }
     }
-  }
+  },
+
+  preprocess: [mdsvex(mdsvexConfig)]
 };
 
 export default config;
