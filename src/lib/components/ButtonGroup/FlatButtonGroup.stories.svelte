7748083c2ab56292f5827867c29cc870643f3637
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-07-03 11:17:41 -0500

    add button group stories

---
+++ src/lib/components/ButtonGroup/FlatButtonGroup.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import FlatButtonGroup from "./FlatButtonGroup.svelte";
+</script>
+
+<Meta title="ButtonGroups/FlatButtonGroup" component={FlatButtonGroup} />
+
+<Template let:args>
+  <FlatButtonGroup {...args} />
+</Template>
+
+<Story name="Default" />
