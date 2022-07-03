1dd572184e3d845352a1141549e1be03caa123ef
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-07-03 11:18:02 -0500

    add CodeScreen wrapper

---
+++ src/lib/components/CodeScreen.stories.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import CodeScreen from "./CodeScreen.svelte";
+</script>
+
+<Meta title="Inro Animations/CodeScreen" component={CodeScreen} />
+
+<Template let:args>
+  <div style="height: 300px;">
+    <CodeScreen {...args}>Oh hello.</CodeScreen>
+  </div>
+</Template>
+
+<Story name="Default" parameters={{ backgrounds: { default: "light" } }} />
---
+++ src/lib/components/CodeScreen.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<div class="bg-neutral-800 shadow h-full w-full rounded-md p-2">
+  <div id="buttons" class="flex flex-row w-full justify-start">
+    <div class="bg-red-500 rounded-full h-3 w-3 mr-2" />
+    <div class="bg-yellow-500 rounded-full h-3 w-3 mr-2" />
+    <div class="bg-green-500 rounded-full h-3 w-3 mr-2" />
+  </div>
+  <div class="py-2 text-xl">
+    <slot />
+  </div>
+</div>
