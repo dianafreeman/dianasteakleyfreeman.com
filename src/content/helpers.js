60ba3db8d3eda0d9323e66911397f2d9cb747ef1
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-10-15 12:46:49 -0500

    rearrange content for seo friendly naming

--- src/content/helpers.js
+++ src/content/helpers.js
@@ -9,36 +9,57 @@ function decorateWithUrlPaths(entries) {
   let newEntries = Object.entries(entries).map(([markdownPath, data]) => {
     // remove period from relative path
     let urlPath = filePathToUrlPath(markdownPath);
-    return [markdownPath, { ...data, metadata: { ...data.metadata, path: urlPath } }];
+    const [category, subcategory, ...rest] = urlPath.split("/").filter(s => s.length > 0)
+    return [markdownPath, { ...data, metadata: { ...data.metadata, category, subcategory } }];
   });
   return Object.fromEntries(newEntries);
 }
 
-function getContentImports(category) {
-  const validDirectories = ["writing", "code", "resources", "gallery"];
-  if (!validDirectories.includes(category)) return [];
-  
+function getContentImports({ category, subcategory }) {
+  // console.log('category',category)
+  const validDirectories = ["blog", "projects", "resources"];
+  if (!validDirectories.includes(category)) return []
+
   // NOTE: `import.meta.globEager` method only accepts string literals
   switch (category) {
-    case "writing":
-      return import.meta.globEager("$content/writing/*");
-    case "code":
-      return import.meta.globEager("$content/code/**/*.md");
+    case "blog":
+      switch (subcategory) {
+        default:
+          return import.meta.globEager("$content/blog/*");
+      }
+    case "projects":
+      switch (subcategory) {
+        case "art":
+          return import.meta.globEager("$content/projects/art/**/*.md");
+        case "code":
+          return import.meta.globEager("$content/projects/code/**/*.md");
+        case "design":
+          return import.meta.globEager("$content/projects/design/**/*.md");
+        default:
+          return import.meta.globEager("$content/projects/**/*.md");
+      }
     case "gallery":
-      return import.meta.globEager("$content/gallery/**/*.md");
+      switch (subcategory) {
+        default:
+          return import.meta.globEager("$content/gallery/**/*.md");
+      }
     case "resources":
-      return import.meta.globEager("$content/resources/**/*.md");
+      switch (subcategory) {
+        default:
+          return import.meta.globEager("$content/resources/**/*.md");
+      }
   }
 }
 
-export async function getEntries(category) {
-  const modules = await getContentImports(category);
+export async function getEntries(categoryParams) {
+  const modules = await getContentImports(categoryParams);
   const modulesWithPaths = decorateWithUrlPaths(modules);
+  // console.log(modulesWithPaths)
   return Object.entries(modulesWithPaths);
 }
 
-export async function getEntry(category, targetPath) {
-  const entries = await getEntries(category);
+export async function getEntry(categoryParams, targetPath) {
+  const entries = await getEntries(categoryParams);
   const entry = entries.filter(([filePath]) => filePath.includes(targetPath))[0];
   return entry;
 }
