63c7550076ea6dbf7ad3f4fba86d7d4eb1dde145
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-10-16 13:04:59 -0500

    trap keyboard focus when the navigation menu is open

--- src/lib/components/Nav/NavBar.svelte
+++ src/lib/components/Nav/NavBar.svelte
@@ -1,48 +1,87 @@
 <script>
   import { writable } from "svelte/store";
-  // import AccessNavToggle from "./AccessNavToggle.svelte";
-  // import NavToggle from "./NavToggle.svelte";
-  import layoutStore from "$stores/LayoutStore";
+  import LayoutStore from "$stores/LayoutStore";
   import { onMount } from "svelte";
   import { slide } from "svelte/transition";
   import Toggle from "../Toggle.svelte";
   import Button from "../Button.svelte";
 
-  let scrollY;
-  let innerWidth;
-  let nav;
-  const menuOpen = writable(false);
+  let navWrapper;
+  let expandedMenu;
+  let focusableContent;
+  let firstFocusableElement;
+  let lastFocusableElement;
 
-  const { setScrollY, setNavHeight } = layoutStore;
-  console.log("top", $layoutStore.navHeight);
-
-  $: setScrollY(scrollY);
+  const focusableElements =
+    'a, button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
 
   const navLinkClasses =
     "text-neutral-300 focus:bg-neutral-800 focus:outline-white hover:bg-neutral-800 px-4 block py-5 text-base font-medium text-center";
   const settingItemClasses =
     "text-neutral-300 px-4 flex justify-between items-center py-5 text-base font-medium";
 
+  const navWrapperClassesClosed = "h-fit";
+  const navWrapperClassesOpen = "h-screen overflow-y-scroll";
+
+  const menuOpen = writable(false);
+  const toggleMenu = () => menuOpen.update((bool) => !bool)
+  const { setNavHeight } = LayoutStore;
+
   onMount(() => {
-    setNavHeight(nav.clientHeight);
+    function onKeyDown(e) {
+      let isTabPressed = e.key === "Tab" || e.keyCode === 9;
+      let isEscPressed = e.key === "Escape" || e.keyCode === 27
+
+      if (isEscPressed){
+        return menuOpen.set(false)
+      }
+      if (!isTabPressed) {
+        return;
+      }
+      if (e.shiftKey) {
+        // if shift key pressed for shift + tab combination
+        if (document.activeElement === firstFocusableElement) {
+          // lastFocusableElement.focus(); // add focus for the last focusable element
+          e.preventDefault();
+        }
+      } else {
+        // if tab key is pressed
+        if ($menuOpen && document.activeElement === lastFocusableElement) {
+          // if focused has reached to last focusable element then focus first focusable element after pressing tab
+          firstFocusableElement.focus(); // add focus for the first focusable element
+          e.preventDefault();
+        }
+      }
+    }
+
+    // Set navigation height so other components can use it
+    // TODO: do we need this?
+    setNavHeight(navWrapper.clientHeight);
+
+    focusableContent = navWrapper.querySelectorAll(focusableElements);
+    firstFocusableElement = focusableContent[0]; // get first element to be focused inside modal
+    lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+
+    document.addEventListener("keydown", onKeyDown);
   });
+
 </script>
 
-<!-- <div class="fixed top-0 left-0 right-0 z-50 {$menuOpen ? 'h-screen bg-neutral-900' : ''}"> -->
 <nav
-  bind:this={nav}
-  class:semi-transparent={scrollY > $layoutStore.navHeight}
-  class="fixed top-0 left-0 right-0 z-50 {$menuOpen
-    ? 'h-screen overflow-y-scroll bg-neutral-900'
-    : ''} relatve w-full h-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 "
+  bind:this={navWrapper}
+  class:bg-neutral-900={$menuOpen}
+  class:semi-transparent={!$menuOpen && $LayoutStore.scrollY > $LayoutStore.navHeight}
+  class="fixed top-0 left-0 right-0 z-50 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 {$menuOpen
+    ? navWrapperClassesOpen
+    : navWrapperClassesClosed}"
 >
-  <div class="flex items-center">
-    <div class="flex-1">
-      <a href="/" class="text-2xl lg:text-3xl xl:text-4xl inline-flex font-bold"
-        >D<span class="text-gray-400">iana</span>.</a
-      >
-    </div>
-    <Button type="button" onClick={() => menuOpen.update((bool) => !bool)}>
+  <div class="flex items-center justify-between">
+    <a href="/" class="text-2xl lg:text-3xl xl:text-4xl inline-flex font-bold"
+      >D<span class="text-gray-400">iana</span>.</a
+    >
+
+    <Button type="button" onClick={toggleMenu}>
       {#if $menuOpen}
         <i class="text-3xl las la-times" />
       {:else}
@@ -50,64 +89,52 @@
       {/if}
     </Button>
   </div>
-  {#if $menuOpen}
-    <div id="menu-container" class="flex flex-col justify-between">
-      <ul
-        class="relative flex flex-col w-full justify-center"
-        transition:slide
-        id="main-menu"
-        style="top: {$layoutStore.navHeight};"
-      >
-        <li>
-          <a href="/" class={navLinkClasses}>home</a>
-        </li>
 
-        {#each layoutStore.validCategories as category}
-          <li>
-            <a href="/{category}" class={navLinkClasses}>{category}</a>
-          </li>
-        {/each}
-      </ul>
+  <div
+    bind:this={expandedMenu}
+    id="main-menu"
+    class:hidden={!$menuOpen}
+    class:flex={$menuOpen}
+    class="flex-col justify-between hidden"
+  >
+    <ul class="relative flex flex-col w-full justify-center">
+      <li>
+        <a href="/" class={navLinkClasses}>home</a>
+      </li>
 
-      <!-- <hr class="border-neutral-500 my-10" /> -->
-      <!-- ACCESS NAV -->
-      <ul class="flex flex-col">
+      {#each LayoutStore.validCategories as category}
         <li>
-          <p
-            href="#"
-            class={settingItemClasses}
-            role="menuitem"
-            tabindex="-1"
-            id="user-menu-item-0"
-          >
-            dyslexia mode
-            <Toggle />
-          </p>
+          <a href="/{category}" class={navLinkClasses}>{category}</a>
         </li>
-        <li>
-          <p
-            href="#"
-            class={settingItemClasses}
-            role="menuitem"
-            tabindex="-1"
-            id="user-menu-item-1"
-          >
-            allow google analytics
-            <Toggle />
-          </p>
-          <p
-            href="#"
-            class="text-neutral-300 hover:text-white text-right block px-3 py-5 text-base font-medium"
-            role="menuitem"
-            tabindex="-1"
-            id="user-menu-item-2"
-          >
-            Clear Settings
-          </p>
-        </li>
-      </ul>
-    </div>
-  {/if}
+      {/each}
+    </ul>
+
+    <!-- <hr class="border-neutral-500 my-10" /> -->
+    <!-- ACCESS NAV -->
+    <ul class="flex flex-col">
+      <li>
+        <p href="#" class={settingItemClasses} role="menuitem" id="user-menu-item-0">
+          dyslexia mode
+          <Toggle />
+        </p>
+      </li>
+      <li>
+        <p href="#" class={settingItemClasses} role="menuitem"  id="user-menu-item-1">
+          allow google analytics
+          <Toggle />
+        </p>
+        <p
+          
+          class="text-neutral-300 hover:text-white text-right block px-3 py-5 text-base font-medium"
+          role="menuitem"
+        
+          id="user-menu-item-2"
+        >
+          Clear Settings
+        </p>
+      </li>
+    </ul>
+  </div>
 </nav>
 
 <!-- </div> -->
