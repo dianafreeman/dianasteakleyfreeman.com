c1f861ab84f6d64aa8eac02e2d68513d47b54679
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-11-22 22:12:24 -0500

    update worfklow file name

--- src/stores/LayoutStore.js
+++ src/stores/LayoutStore.js
@@ -1,14 +1,10 @@
-import { readable, writable } from "svelte/store";
+import { writable } from "svelte/store";
 
 export const navHeight = writable(0);
 export const scrollY = writable(0);
 export const mainMenuIsOpen = writable(false);
 export const settingsMenuIsOpen = writable(false);
 export const pageHeadings = writable([]);
-export const classes = readable({
-  RESPONSIVE_CONTAINER: "",
-  MAIN_HEADING: "",
-  SUB_HEADING: ""
-});
+
 
 export const isMobileScreen = writable(false);
