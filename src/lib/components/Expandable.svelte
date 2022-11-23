79bcbcfbe8245d50e151a20dbd2a770afafdc072
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-11-23 10:49:54 -0500

    add expandables to landing page

---
+++ src/lib/components/Expandable.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Button from "./Button.svelte";
+
+
+  /** @type { string } */
+  export let title;
+  /** @type { "p"  | "h1" | "h2" | "h3" | "h4"} */
+  export let titleElement = "p";
+
+  let isOpen = false;
+</script>
+
+<div class="flex flex-col w-auto">
+  <Button on:click={() => (isOpen = !isOpen)} class=" my-2 py-3 flex justify-between items-center">
+    <svelte:element this={titleElement} class="text-2xl font-thin mx-2 ">
+      {title}
+    </svelte:element>
+    <i aria-hidden="true" class="mx-2 text-light-gray text-3xl las la-angle-{isOpen ? 'up' : 'down'}" />
+  </Button>
+  {#if isOpen}
+    <!-- <div class="bg-darkest-gray text-sm"> -->
+    <div class="text-sm">
+      <slot />
+    </div>
+  {/if}
+</div>
--- src/routes/resources/+page.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-  <title>Resources | Diana M Steakley-Freeman</title>
-</svelte:head>
-
-<div class="flex h-screen w-full items-center justify-center align-middle">
-  <h1 class="w-3/4 text-3xl">More resources coming soon.</h1>
-</div>
