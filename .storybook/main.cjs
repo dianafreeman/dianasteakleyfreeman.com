8da51ddaf2fc5edaa6f252c88f56afc5f4751816
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-10 14:18:03 -0600

    allow aliases in storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,7 @@
+const preprocess = require('svelte-preprocess');
+const path = require('path');
+const { loadConfigFromFile, mergeConfig } = require('vite');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,7 +10,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-svelte-csf"
   ],
   "framework": "@storybook/svelte",
   "core": {
@@ -16,6 +21,18 @@ module.exports = {
     "preprocess": import("../svelte.config.js").preprocess
   },
   "features": {
-    "storyStoreV7": true
-  }
+    "storyStoreV7": false
+  },
+  async viteFinal(config, { configType }) {
+		const { config: userConfig } = await loadConfigFromFile(
+			path.resolve(__dirname, "../vite.config.js")
+		);
+		// Remove Svelte plugins that would duplicate those added by the Storybook plugin
+		const plugins = userConfig.plugins.flat(1)
+			.filter(p => !p.name.startsWith('vite-plugin-svelte') || p.name === 'vite-plugin-svelte-kit');
+		return mergeConfig(config, {
+			...userConfig,
+			plugins
+		});
+  	}
 }
---
+++ src/stories/Button.stories.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+
+  import Button from "../lib/components/Button.svelte";
+</script>
+
+<Meta title="Button" component={Button} />
+
+<Story name="Primary">
+  <Button class="p-5">Hello World </Button>
+</Story>
