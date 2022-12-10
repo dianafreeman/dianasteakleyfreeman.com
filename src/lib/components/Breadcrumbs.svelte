d1b05ebadea74fbeafcc7baa3493b1cdd66bea0c
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-10 12:59:16 -0600

    add a darkest gray

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -10,7 +10,7 @@
 </script>
 
 {#if !DISABLED_PAGES.includes($page.url.pathname)}
-  <nav class="relative bg-darkest-gray px-4 py-2 {clazz}" aria-label="Breadcrumb">
+  <nav class="relative px-4 py-2 {clazz}" aria-label="Breadcrumb">
     <ol class="mx-1 flex list-none gap-2">
       {#each items as item}
         <BreadcrumbItem current={item.relativePath === $page.url.pathname} {item} />
