26b16a82d46986119e62330a23b7fd0b76c5ab52
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-11 12:09:33 -0600

    add privacy policy

---
+++ src/routes/privacy-policy/+page.js
@@ -0,0 +1,15 @@
+
+import { error } from "@sveltejs/kit";
+
+export async function load({ params }) {
+    const [, module] = Object.entries(import.meta.glob("./index.md")).flat();
+    const entry =  await module()
+    if (entry) {
+    return {
+      entry,
+      title: "Privacy Policy"
+    };
+  } else {
+    throw error(500, "Privacy Policy Page Endpoint Failed")
+  }
+}
