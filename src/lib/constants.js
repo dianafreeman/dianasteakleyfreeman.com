9a0ca11a807d9480ec5b8355d9acd03a656a103d
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-18 13:47:08 -0600

    add heading styles to app.css

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,2 +1,2 @@
-export const HEADING_CLASSES = "text-6xl md:text-7xl lg:text-8xl";
+export const HEADING_CLASSES = "text-5xl md:text-6xl lg:text-7xl";
 export const SUBHEADING_CLASSES = "text-xl md:text-2xl lg:text-3xl font-thin";
