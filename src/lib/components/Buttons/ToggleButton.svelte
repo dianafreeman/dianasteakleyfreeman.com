cf4d5f979f9e57f77375fac2c8c20dc271576d3d
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-20 21:24:03 -0600

    add string helpers

---
+++ src/lib/components/Buttons/ToggleButton.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import Button from "./Button.svelte";
+
+  /** @type { string } */
+  export let label;
+  /** @type { string } */
+  export let value;
+  /** @type { boolean } */
+  export let isActive;
+  /** @type { boolean } */
+  export let isPending;
+  /** @type { boolean } */
+  // export let useSwitch;
+
+  const dispatch = createEventDispatcher();
+
+  function handleClick() {
+    dispatch("click", { label, value });
+  }
+
+  $: icon = isPending ? "las la-plus" : isActive ? "las la-check" : "";
+</script>
+
+<Button
+  on:click={handleClick}
+  class="my-2 flex items-center justify-end p-3 text-lg {isActive
+    ? 'bg-gray'
+    : ''}"
+  iconClass="mx-5 {icon}"
+  elementProps={{
+    "aria-pressed": isPending,
+    "aria-label": `${label} ${isActive ? "(applied)" : ""}`,
+    value,
+    name: "categoryFilter"
+  }}>
+  {label.toLowerCase()}
+</Button>
