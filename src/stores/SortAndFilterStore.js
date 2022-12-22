4da63fce88dd926785131b4f3bdfaa2e94218683
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-21 22:06:40 -0600

    refine sort and filter store

--- src/stores/SortAndFilterStore.js
+++ src/stores/SortAndFilterStore.js
@@ -8,14 +8,7 @@ export function reduceEntriesTo(key, entries) {
   return Array.from(
     new Set(
       entries.reduce((accum, e) => {
-        // console.log(e.metadata)
-        if (!Object.keys(e.metadata).includes(key))
-          throw new Error(
-            `ERROR. Can't reduce with an invalid key. "${key}" not found in the object. `
-          );
         const values = e.metadata[key];
-
-        // console.log(Array.isArray(values))
         if (!values) return accum;
         if (Array.isArray(values)) {
           return [...accum, ...values];
@@ -27,39 +20,49 @@ export function reduceEntriesTo(key, entries) {
   );
 }
 
-function hasMatchingTags(entry, allTags){
-  const matchedTags = entry.metadata.tags.map(t => allTags.includes(t))
-  return matchedTags.length !== 0
-}
-
-
 function createSortAndFilterStore(entryArray) {
   if (!entryArray) return;
 
-  const tagFilters = writable([]);
-  const categoryFilter = writable("");
+  const tags = writable([]);
+  const category = writable({});
 
-
-  const tags = reduceEntriesTo("tags", entryArray).map((tag) => ({
+  const allTags = reduceEntriesTo("tags", entryArray).map((tag) => ({
     value: stripAllButSpacesAndAlphaNumeric(tag),
     label: tag
   }));
 
-  const categories = reduceEntriesTo("category", entryArray).map(
+  const allCategories = reduceEntriesTo("category", entryArray).map(
     (categorySlug) => ({
       value: categorySlug,
-      label: kebabCaseToTitleCase(categorySlug).replace("Usa", "USA")
+      label: kebabCaseToTitleCase(categorySlug)
     })
   );
 
-  const filteredItems = derived([tagFilters, categoryFilter], ([$tags, $category]) => {
-    if (!$tags.length && !$category) return entryArray
-    
-    return entryArray.filter(entry => hasMatchingTags(entry, $tags)).filter( entry => $category === entry.metadata.category)
-  })
+  function entryMatchesTags(entry, tagsObject) {
+    const matchedTags = entry.metadata.tags.filter((t) => tagsObject.map(v => v.label).includes(t));
+    return matchedTags.length !== 0;
+  }
+  
+  function entryMatchesCategory(entry, categoryObject){
+    if (!categoryObject || categoryObject.value === "") return true 
+    const categoryRegex =  new RegExp(categoryObject.value, "i")
+    return entry.metadata.category.match(categoryRegex)
+  }
+  
+  const filteredItems = derived(
+    [tags, category],
+    ([$tags, $category]) => {
+      if (!$tags.length && !$category.value) return entryArray;
+
+      return entryArray.filter( e => $category.value ? entryMatchesCategory(e, $category) : true).filter(e => $tags.length ? entryMatchesTags(e, $tags) : true)
+       
+    }
+  );
+
+  filteredItems.subscribe(v => console.log("filteredItems", v))
 
   function toggleFilter(term) {
-    tagFilters.update((currentFilters) => {
+    tags.update((currentFilters) => {
       // remove the term if it exists
       if (currentFilters.includes(term)) {
         return currentFilters.filter((v) => v !== term);
@@ -70,17 +73,16 @@ function createSortAndFilterStore(entryArray) {
   }
 
   function clearFilters() {
-    tagFilters.set([]);
-    categoryFilter.set("")
+    tags.set([]);
+    category.set();
   }
 
-  
   return {
     subscribe: filteredItems.subscribe,
-    items: filteredItems,
-    filters: { categories, tags },
-    tagFilters,
-    categoryFilter,
+    results: filteredItems,
+    options: { categories: allCategories, tags:allTags },
+    tags,
+    category,
     toggleFilter,
     clearFilters
   };
