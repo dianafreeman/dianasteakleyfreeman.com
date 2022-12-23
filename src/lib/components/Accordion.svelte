4279955724d2f848abc64feb5810f90bd0306d09
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-22 21:37:53 -0600

    create accordion

---
+++ src/lib/components/Accordion.svelte
@@ -0,0 +1,60 @@
+<script>
+  import Button from "./Buttons/Button.svelte";
+
+  /** @type { string } */
+  export let title;
+  /** @type { "p"  | "h1" | "h2" | "h3" | "h4"} */
+  export let titleElement = "p";
+  /** @type { boolean } */
+  export let isOpen = false;
+
+  $: iconClasses = isOpen ? "la-angle-up" : "la-angle-down";
+</script>
+
+<dl class="divide-gray-200 mt-6 space-y-6 divide-y">
+  <div class="pt-6">
+    <dt class="text-lg">
+      <Button
+        type="button"
+        animateBorders={false}
+        class="p-5 text-gray-400 flex w-full items-start border-b-0 justify-between text-left {isOpen ? "bg-darkest-gray" : ""}"
+        aria-controls="faq-0"
+        aria-expanded={isOpen}
+        on:click={() => (isOpen = !isOpen)}
+      >
+        <svelte:element this={titleElement} class="text-gray-900 font-medium"
+          >{title}</svelte:element
+        >
+        <span class="ml-6 flex h-7 items-center">
+          <i class="las {iconClasses}" />
+        </span>
+      </Button>
+    </dt>
+    {#if isOpen}
+      <dd class="mb-2 p-5 border border-t-0 border-gray" id="faq-0">
+        <p class="text-gray-500 text-base">
+          I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quas cupiditate laboriosam fugiat.
+        </p>
+      </dd>
+    {/if}
+  </div>
+
+  <!-- More questions... -->
+</dl>
+<!-- <div class="flex w-auto flex-col">
+  <Button on:click={() => (isOpen = !isOpen)} class=" my-2 flex items-center justify-between py-3">
+    <svelte:element this={titleElement} class="mx-2 text-2xl font-thin ">
+      {title}
+    </svelte:element>
+    <i
+      aria-hidden="true"
+      class="las mx-2 text-3xl text-light-gray la-angle-{isOpen ? 'up' : 'down'}"
+    />
+  </Button>
+  {#if isOpen}
+    <div class="text-sm">
+      <slot />
+    </div>
+  {/if}
+</div> -->
---
+++ src/stories/Accordion.stories.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+
+  import Accordion from "../lib/components/Accordion.svelte";
+</script>
+
+<Meta title="Accordion" component={Accordion} />
+
+<Story name="collapsed">
+  <Accordion class="p-5" title="This is an accordion" isOpen={false}
+    >Hello from an accordion
+  </Accordion>
+</Story>
+
+<Story name="expanded">
+  <Accordion class="p-5" title="This is an accordion" titleElement={"h3"} isOpen
+    >Hello from an accordion
+  </Accordion>
+</Story>
