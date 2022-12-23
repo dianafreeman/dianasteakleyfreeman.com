0a53ae33e228ff80c025ce90cc76836e4b848bff
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-23 15:06:26 -0600

    add reusable templates

---
+++ __templates_/Component.stories.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Component from "$lib/components/Component.svelte";
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import testData from "./testData/entries.json";
+</script>
+
+<Meta title="Component" component={Component} />
+
+<Template let:args>
+  <Component data={args.data} />
+</Template>
+
+<Story name="Primary" args={{ data: testData }} />
