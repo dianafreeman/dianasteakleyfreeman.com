9ebf86d487a8682446ad41a7a89170e9052d32f7
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-23 15:19:08 -0600

    format form

--- src/lib/components/Forms/ContactForm.svelte
+++ src/lib/components/Forms/ContactForm.svelte
@@ -1,46 +1,72 @@
 <script>
-
 </script>
+
 <form class="w-full max-w-lg">
-  <div class="flex flex-wrap -mx-3 mb-6">
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+  <div class="-mx-3 mb-6 flex flex-wrap">
+    <div class="mb-6 w-full px-3 md:mb-0 md:w-1/2">
+      <label
+        class="text-gray-700 mb-2 block text-xs font-bold uppercase tracking-wide"
+        for="grid-first-name">
         First Name
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
-      <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+      <input
+        class="bg-gray-200 text-gray-700 mb-3 block w-full appearance-none rounded border border-red-500 py-3 px-4 leading-tight focus:bg-white focus:outline-none"
+        id="grid-first-name"
+        type="text"
+        placeholder="Jane" />
+      <p class="text-xs italic text-red-500">Please fill out this field.</p>
     </div>
-    <div class="w-full md:w-1/2 px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+    <div class="w-full px-3 md:w-1/2">
+      <label
+        class="text-gray-700 mb-2 block text-xs font-bold uppercase tracking-wide"
+        for="grid-last-name">
         Last Name
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+      <input
+        class="bg-gray-200 text-gray-700 border-gray-200 focus:border-gray-500 block w-full appearance-none rounded border py-3 px-4 leading-tight focus:bg-white focus:outline-none"
+        id="grid-last-name"
+        type="text"
+        placeholder="Doe" />
     </div>
   </div>
-  <div class="flex flex-wrap -mx-3 mb-6">
+  <div class="-mx-3 mb-6 flex flex-wrap">
     <div class="w-full px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+      <label
+        class="text-gray-700 mb-2 block text-xs font-bold uppercase tracking-wide"
+        for="grid-password">
         E-mail
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email">
+      <input
+        class="bg-gray-200 text-gray-700 border-gray-200 focus:border-gray-500 mb-3 block w-full appearance-none rounded border py-3 px-4 leading-tight focus:bg-white focus:outline-none"
+        id="email"
+        type="email" />
       <p class="text-gray-600 text-xs italic">Some tips - as long as needed</p>
     </div>
   </div>
-  <div class="flex flex-wrap -mx-3 mb-6">
+  <div class="-mx-3 mb-6 flex flex-wrap">
     <div class="w-full px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+      <label
+        class="text-gray-700 mb-2 block text-xs font-bold uppercase tracking-wide"
+        for="grid-password">
         Message
       </label>
-      <textarea class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
-      <p class="text-gray-600 text-xs italic">Re-size can be disabled by set by resize-none / resize-y / resize-x / resize</p>
+      <textarea
+        class=" no-resize bg-gray-200 text-gray-700 border-gray-200 focus:border-gray-500 mb-3 block h-48 w-full resize-none appearance-none rounded border py-3 px-4 leading-tight focus:bg-white focus:outline-none"
+        id="message" />
+      <p class="text-gray-600 text-xs italic">
+        Re-size can be disabled by set by resize-none / resize-y / resize-x /
+        resize
+      </p>
     </div>
   </div>
   <div class="md:flex md:items-center">
     <div class="md:w-1/3">
-      <button class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+      <button
+        class="focus:shadow-outline rounded bg-teal-400 py-2 px-4 font-bold text-white shadow hover:bg-teal-400 focus:outline-none"
+        type="button">
         Send
       </button>
     </div>
-    <div class="md:w-2/3"></div>
+    <div class="md:w-2/3" />
   </div>
 </form>
