ec3e1fe8287bec60bfd48b094198ec469aad1e7f
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-23 19:21:53 -0600

    add a contact page and form

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import LandingSection from "$lib/components/LandingSection.svelte";
+  import { default as ContactForm } from "$lib/components/Forms/ContactForm.svelte";
+  import Typewriter from "svelte-typewriter/Typewriter.svelte";
+</script>
+
+<LandingSection fadeInWhenVisible={false} class="relative justify-center ">
+  <h1 class="heading mx-auto w-2/3 text-center lowercase">let's connect.</h1>
+
+  <ContactForm />
+</LandingSection>
