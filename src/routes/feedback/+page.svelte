708774cd65aece4c93cf365d9e9763107d18f6b5
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-26 15:42:37 -0600

    Add a feedback page

---
+++ src/routes/feedback/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import LandingSection from "$lib/components/LandingSection.svelte";
+  import { default as FeedbackForm } from "$lib/components/Forms/FeedbackForm.svelte";
+</script>
+
+<LandingSection fadeInWhenVisible={false} class="relative justify-center ">
+  <FeedbackForm title="Let's Connect" />
+</LandingSection>
