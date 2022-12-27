a035e7f486722cbea891764fe6e47b0ce882b46a
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-27 17:14:35 -0600

    fix vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,19 +1,16 @@
-
-import path from 'path'
-import { defineConfig, loadConfigFromFile } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from "path";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-  plugins: [
-    svelte({ hot: !process.env.VITEST }),
-  ],
+  plugins: [svelte({ hot: !process.env.VITEST })],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom"
   },
   resolve: {
-    alias: { 
-      "$lib": path.resolve(__dirname, "./src/lib")
+    alias: {
+      $lib: path.resolve(__dirname, "./src/lib")
     }
   }
-})
+});
