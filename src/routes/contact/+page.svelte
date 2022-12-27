7b62475f9273be1f977f376da4cc108f3161a1cc
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-12-27 15:36:46 -0600

    add contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,8 +1,7 @@
 <script>
-  import LandingSection from "$lib/components/LandingSection.svelte";
   import { default as ContactForm } from "$lib/components/Forms/ContactForm.svelte";
 </script>
 
-<LandingSection fadeInWhenVisible={false} class="relative justify-center ">
+<div class="relative justify-center ">
   <ContactForm title="Let's Connect" />
-</LandingSection>
+</div>
