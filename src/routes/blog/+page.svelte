5f0ba6bd188e13706200c9bbc08cf23843fe425b
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2024-11-09 22:40:44 -0600

    add project and blog pages

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,9 +1,18 @@
 <script>
+  import HorizontalCard from "$lib/components/Cards/HorizontalCard.svelte";
+
   export let data;
 </script>
 
-{#each data.entries as entry}
-  <pre>
-    {JSON.stringify(entry, null, 2)}
-  </pre>
-{/each}
+<div class="pb-10">
+  <h1 class="text-3xl md:text-5xl font-bold text-center">Blog</h1>
+</div>
+
+<div class="w-full px-10 flex flex-wrap justify-center">
+  {#each data.entries as entry}
+    <HorizontalCard
+      title={entry.metadata.title}
+      description={entry.metadata.description}
+      imageSrc={entry.metadata.image} />
+  {/each}
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-  import HorizontalCard from "$lib/components/Cards/HorizontalCard.svelte";
+  import Masonry from "$lib/components/Masonry.svelte";
 
   export let data;
 </script>
 
-{#each data.entries as entry}
-  <HorizontalCard title={entry.metadata.title} imageSrc="localhost:5173/{entry.metadata.image}" />
-  <pre>
-    {JSON.stringify(entry, null, 2)}
-  </pre>
-{/each}
+<div class="pb-10">
+  <h1 class="text-3xl md:text-5xl font-bold text-center">Projects</h1>
+</div>
+<div>
+  <Masonry items={data.entries} />
+</div>
