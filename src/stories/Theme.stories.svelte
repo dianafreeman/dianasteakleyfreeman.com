bc4cfc7c7b5de1ea5bd5607063cee4e5cb259693
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2024-11-11 12:42:43 -0600

    replace darkest-gray with dark-gray

--- src/stories/Theme.stories.svelte
+++ src/stories/Theme.stories.svelte
@@ -6,7 +6,7 @@
 
 <Story name="Dark">
   <div class="bg-black p-5">black</div>
-  <div class="bg-darkest-gray p-5">darkest-gray</div>
+  <div class="bg-dark-gray p-5">dark-gray</div>
   <div class="bg-dark-gray p-5">dark-gray</div>
   <div class="bg-gray p-5">gray</div>
   <div class="bg-light-gray p-5">light-gray</div>
