4e1538db251277c22fc95a6d906396f4d5032006
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2024-11-13 19:47:19 -0600

    add update deps via sv command line, for future refs

---
+++ new/tailwind.config.js
@@ -0,0 +1,13 @@
+import aspectRatio from '@tailwindcss/aspect-ratio';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [forms, aspectRatio]
+};
