a30ff1125ea4193dfbd753d47f2a680a37add319
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2024-12-25 20:42:58 -0600

    don't use GA during build, only in browser

--- src/lib/components/GoogleAnalytics.svelte
+++ src/lib/components/GoogleAnalytics.svelte
@@ -17,12 +17,15 @@
 
   const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
 
-  // TODO - setup consent
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  if (browser){
 
-  gtag('config', measurementId);
+    // TODO - setup consent
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    
+    gtag('config', measurementId);
+  }
 </script>
 
 <svelte:head>
